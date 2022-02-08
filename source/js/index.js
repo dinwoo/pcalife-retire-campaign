5a367f46b0bd28d2fcaeaea6e65e24e2f0f4397d
dinwoo <danielforboy@gmail.com>
2022-02-09 00:49:52 +0800

    feat: 切版

--- source/js/index.js
+++ source/js/index.js
@@ -1,21 +1,70 @@
-let fun = () => {
-  $(document).ready(function () {
-    $("#carousel-box").owlCarousel({
+$(window).resize(function () {
+  let windowsize = $(window).width();
+  console.log(windowsize);
+  let prodcutOwl = $("#product-carousel");
+  if (windowsize < 768) {
+    prodcutOwl.owlCarousel({
       nav: false,
       dots: true,
       responsive: {
         0: {
           loop: true,
-          items: 1,
-        },
-        769: {
-          loop: false,
           items: 3,
-          margin: 100,
         },
       },
     });
-  });
-};
+    $("#productLeftArrow").on("click", () => {
+      prodcutOwl.trigger("prev.owl.carousel");
+    });
+    $("#productRightArrow").on("click", () => {
+      prodcutOwl.trigger("next.owl.carousel");
+    });
+  } else {
+    prodcutOwl.trigger("destroy.owl.carousel");
+  }
+});
 
-fun();
+$(document).ready(function () {
+  let prodcutOwl = $("#product-carousel");
+  prodcutOwl.owlCarousel({
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        loop: true,
+        items: 1,
+      },
+    },
+    onChanged: (event) => {
+      if (event.page.index < 0) {
+        $("#productNowPage").text(`01`);
+      } else {
+        $("#productNowPage").text(`0${event.page.index + 1}`);
+      }
+    },
+  });
+  $("#productLeftArrow").on("click", () => {
+    prodcutOwl.trigger("prev.owl.carousel");
+  });
+  $("#productRightArrow").on("click", () => {
+    prodcutOwl.trigger("next.owl.carousel");
+  });
+  $("#carousel-box").owlCarousel({
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        loop: true,
+        items: 1,
+      },
+      769: {
+        loop: false,
+        items: 3,
+        margin: 50,
+      },
+      1280: {
+        margin: 100,
+      },
+    },
+  });
+});
