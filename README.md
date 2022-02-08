a58c7ba630f5a554e77b425b6265528b49d6da43
Daniel Wu <daniel.wu@language-center.com.tw>
2022-02-08 17:46:44 +0800

    Update 2022-02-08T09:46:37.558Z

--- README.md
+++
@@ -1,1 +0,0 @@
-# pcalife-retire-campaign
---
+++ js/all.js
@@ -0,0 +1,2 @@
+"use strict";var fun=function(){$(document).ready(function(){$("#carousel-box").owlCarousel({nav:!1,dots:!0,responsive:{0:{loop:!0,items:1},769:{loop:!1,items:3,margin:100}}})})};fun();
+//# sourceMappingURL=all.js.map
